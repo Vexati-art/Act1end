f4eef6408a6f41bd836fd61a1b58bf06388d3a2b
Vexati-art <55270612+Vexati-art@users.noreply.github.com>
2020-03-05 18:44:39 +0800

    Update form_update.php

--- form_update.php
+++ form_update.php
@@ -21,24 +21,23 @@
         <a href="index.php?navi=create">Create</a>
         <a href="index.php?navi=delete">Delete</a>
     </div>
-	<h1> Update</h1>
-<form action="pro_update.php?id=<?php echo $id ?>" method="POST">
-	<input type="text" name="name" value="<?php echo $result['name'];?>"/>
-	<input type="text" name="description" value="<?php echo $result['description']; ?>"/>
-	<input type="text" name="price" value="<?php echo $result['price']; ?>"/>
-	<select name="category">
-	<option value="<?php echo $result['category_id'];?>"><?php echo $result['category_name'];?></option>
-		<?php
-		foreach($category as $cview){
-		?>
-			<option value="<?php echo $cview['id']?>"><?php echo $cview['name']?></option>
-		<?php
-		}
-		?>
-		</select>
-	<input type="submit" name="submit" value="submit"/>
+	<h1> Update Product</h1>
+<form class="form-inline" action="pro_create.php" method="POST">
+<input type="text" name="name" placeholder="name"/>
+<input type="text" name="description" placeholder="description"/>
+<input type="text" name="price" placeholder="price"/>
+<select name="category">
+<option value="">--Category--</option>
+	<?php
+      foreach($category as $cview){
+    ?>
+		<option value="<?php echo $cview['id']?>"><?php echo $cview['name']?></option>
+    <?php
+      }
+    ?>
+	</select>
+<input type="submit" name="submit" value="submit"/>
 
 </form>
-
 
 </html>
